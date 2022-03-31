d69ea249440ee58d264612175c0c63d3cf607c9f
Yurec <boomy50@mail.ru>
2022-04-01 05:12:05 +0900

    add product

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+#from django.db import models #возможно потребуется разкоментить
 
-# Create your models here.
+from pyexpat import model
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Products(models.Model):
+    product_name = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    storage_count = models.PositiveSmallIntegerField()
+    category = models.CharField(max_length=50)
+    product_img = models.URLField()
+    description = models.TextField()
